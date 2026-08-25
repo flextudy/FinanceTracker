daeb648848bc3af623f5f1cb2333f2668a74394d
Aditya Sharma <adityas20032005@gmail.com>
2026-08-25 19:36:26 +0530

    Settlements API

---
+++ app/api/settlements/route.ts
@@ -0,0 +1,86 @@
+import { SettlementStatus } from "@/app/generated/prisma/enums";
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request:Request){
+    try{
+        const body = await request.json();
+
+        const {fromUserId,toUserId,amount} = body;
+
+        if(!fromUserId || !toUserId || typeof amount !== "number" || amount <= 0){
+            return NextResponse.json(
+                {error:"Invalid Settlement Data"},
+                {
+                    status:400
+                }
+            )
+        }
+
+        if(fromUserId === toUserId){
+            return NextResponse.json(
+                {error:"A user cannot settle with themselves"},
+                {
+                    status:400
+                }
+            )
+        }
+
+        const amountPaid = Math.round(amount);
+        const users = await prisma.user.findMany({
+            where:{
+                id:{
+                    in:[fromUserId,toUserId]
+                }
+            }
+        })
+
+        if(users.length !== 2){
+            return NextResponse.json(
+                {error:"Invalid Users"},
+                {
+                    status:404
+                }
+            )
+        }
+
+        const settlement = await prisma.settlement.create({
+            data:{
+                fromUserId,
+                toUserId,
+                amountPaid,
+                status: SettlementStatus.COMPLETED,
+                settledAt:new Date(),
+            },
+            include:{
+                fromUser:{
+                    select:{
+                        id:true,
+                        name:true
+                    }
+                },
+                toUser:{
+                    select:{
+                        id:true,
+                        name:true
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json({
+            settlement
+        })
+
+        
+    }
+    catch(error){
+        console.log(error);
+        return NextResponse.json(
+            {error:"Failed to create settlement"},
+            {
+                status:500
+            }
+        )
+    }
+}
